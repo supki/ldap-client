bab975ef06c01e35655ff4c36474e898cb8ea31e
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-06-08 20:24:37 +0000

    Nix configuration

---
+++ ldap-client.nix
@@ -0,0 +1,17 @@
+{ mkDerivation, asn1-encoding, asn1-types, async, base, bytestring
+, connection, containers, doctest, hspec, network, process
+, semigroups, stdenv, stm, text
+}:
+mkDerivation {
+  pname = "ldap-client";
+  version = "0.1.0";
+  src = ./.;
+  buildDepends = [
+    asn1-encoding asn1-types async base bytestring connection
+    containers network semigroups stm text
+  ];
+  testDepends = [ base bytestring doctest hspec process semigroups ];
+  homepage = "https://supki.github.io/ldap-client";
+  description = "Pure Haskell LDAP Client Library";
+  license = stdenv.lib.licenses.bsd2;
+}
