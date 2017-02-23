87a9b3b26e0b8e690729c220d9fa1ca4103bd8a5
Matvey Aksenov <matvey.aksenov@gmail.com>
2017-02-23 20:38:20 +0000

    travis please

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }: let
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }: let
   ghc = nixpkgs.pkgs.haskell.packages.${compiler};
   npm = import ./npm {};
 in
