c98518ba97afc5548792af270fb2ce161a7a4cf6
Matvey Aksenov <matvey.aksenov@gmail.com>
2017-02-23 21:18:34 +0000

    Maintenance.

--- CHANGELOG.markdown
+++ CHANGELOG.markdown
@@ -1,6 +1,8 @@
 next
 ====
 
+  * Supported SASL authentication via the EXTERNAL mechanism. (https://github.com/supki/ldap-client/pull/9)
+
   * Added the `SecureWithTLSSettings` constructor to the `Host` datatype for the
     cases where the user needs more control over TLS connection settings.
     (https://github.com/supki/ldap-client/issues/5, https://github.com/supki/ldap-client/pull/6)
--- shell.nix
+++ shell.nix
@@ -1,17 +1,23 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }: let
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }: let
   inherit (nixpkgs) pkgs;
-  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages(ps: [
+  haskell = pkgs.haskell.packages.${compiler};
+
+  ghc = haskell.ghcWithPackages(ps: [
     ps.hdevtools ps.doctest ps.hspec-discover ps.hlint ps.ghc-mod
   ]);
-  cabal-install = pkgs.haskell.packages.${compiler}.cabal-install;
-  pkg = import ./default.nix { inherit nixpkgs compiler; };
   npm = import ./npm {};
+
+  this = import ./default.nix { inherit nixpkgs compiler; };
 in
   pkgs.stdenv.mkDerivation rec {
-    name = pkg.pname;
-    buildInputs = [ ghc cabal-install npm.nodePackages.ldapjs ] ++ pkg.env.buildInputs;
+    name = this.pname;
+    buildInputs = [
+      ghc
+      haskell.cabal-install
+      npm.nodePackages.ldapjs
+    ] ++ this.env.buildInputs;
     shellHook = ''
-      ${pkg.env.shellHook}
+      ${this.env.shellHook}
       cabal configure --enable-tests --package-db=$NIX_GHC_LIBDIR/package.conf.d
     '';
   }
